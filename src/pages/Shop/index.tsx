ee42dc538ea66dd0cd4762bd2ee8d55fd2129fec
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-06-03 09:51:29 +0100

    shop

--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Shop = () => {
   return (
     <div className=" mt-20 h-[400px] w-full flex flex-col  gap-8 items-center justify-center ">
-      <h1>Shop</h1>
+      <h1>Shopuioo</h1>
       <Link to="/">
         <button className="rounded-md bg-black text-white py-3 px-4 pb-4 ">
           GO TO HOMEPAGE
